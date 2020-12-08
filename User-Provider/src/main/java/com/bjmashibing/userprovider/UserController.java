d4a82482923de70fcbc7759ea945bc5f60b8d645
yewei <20751268@qq.com>
2020-12-08 19:14:02 +0800

    hystrix

--- User-Provider/src/main/java/com/bjmashibing/userprovider/UserController.java
+++ User-Provider/src/main/java/com/bjmashibing/userprovider/UserController.java
@@ -1,7 +1,10 @@
 package com.bjmashibing.userprovider;
 
 import com.bjmashibing.userapi.UserApi;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class UserController implements UserApi {
@@ -12,7 +15,37 @@ public class UserController implements UserApi {
     }
 
     @Override
-    public String getById(Integer Id) {
+    public String getById(Integer id) {
+        // TODO Auto-generated method stub
         return null;
     }
+
+    @GetMapping("/getMap")
+    public Map<Integer, String> getMap(@RequestParam("id") Integer id) {
+        // TODO Auto-generated method stub
+        System.out.println(id);
+        return Collections.singletonMap(id, "mmeme");
+    }
+    @GetMapping("/getMap2")
+    public Map<Integer, String> getMap2(Integer id,String name) {
+        // TODO Auto-generated method stub
+        System.out.println(id);
+        return Collections.singletonMap(id, name);
+    }
+
+    @GetMapping("/getMap3")
+    public Map<Integer, String> getMap3(@RequestParam Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        System.out.println(map);
+        return Collections.singletonMap(Integer.parseInt(map.get("id").toString()), map.get("name").toString());
+    }
+
+
+    @PostMapping("/postMap")
+    public Map<Integer, String> postMap(@RequestBody Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        System.out.println(map);
+        return Collections.singletonMap(Integer.parseInt(map.get("id").toString()), map.get("name").toString());
+    }
+
 }
