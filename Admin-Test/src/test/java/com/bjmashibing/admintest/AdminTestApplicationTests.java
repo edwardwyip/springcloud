498d7f78dab489a737d10242555b9fa41520ece4
yewei <20751268@qq.com>
2020-12-09 16:51:21 +0800

    sleuth+zipkin，spring boot admin

---
+++ Admin-Test/src/test/java/com/bjmashibing/admintest/AdminTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjmashibing.admintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
